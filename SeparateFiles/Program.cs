80c19cb69288becab2921105e1270bf79e3cb120
Kamil <pawelskikamil9@gmail.com>
2023-03-15 21:33:48 +0100

    Size of new the folders

--- SeparateFiles/Program.cs
+++ SeparateFiles/Program.cs
@@ -1,44 +1,74 @@
-﻿class Program
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Enumeration;
+
+//Program is not ready
+//to do
+//the size of the folders must be larger than the largest file size
+//loops and exceptions to paths that exist? or something like this
+//copying to folders 
+//more and more i need to think :O
+class Program
 {
     static void Main(string[] args)
     {
-        FileInfo[] filesPath;
+        FileInfo[] files;
         DirectoryInfo directory;
-        while (true) { 
+        DirectoryInfo newDirectory;
+        ulong directorySize;
+        ulong newSize;
+        ushort folderCount;
+        Console.WriteLine("Enter directory path.");
+        var path = Console.ReadLine() ?? string.Empty;
+        directory = Directory.CreateDirectory(path);
+        files = directory.GetFiles();
+        directorySize = DirectorySize(directory); 
+        Console.WriteLine(DirectorySize(directory));
+        Console.WriteLine("Enter how much space you want for the folders (B)");
+        while (true)
+        {
             try
             {
-                Console.WriteLine("Enther the files path");
-                directory = new DirectoryInfo(Console.ReadLine());
-                filesPath =directory.GetFiles();
-                foreach (var x in filesPath) {
-                    Console.WriteLine(x);
-                }
+                var size = Console.ReadLine() ?? string.Empty;
+                newSize = ulong.Parse(size);
                 break;
             }
-            catch (DirectoryNotFoundException) 
+            catch (FormatException)
             {
-                Console.WriteLine("Error - Directory not found.");
+                Console.WriteLine("Error - Wrong format");
             }
-
         }
-
-        Console.WriteLine(DirSize(directory));
+        folderCount = (ushort)(Math.Ceiling((double)(directorySize / newSize)));
+        Console.WriteLine("Enter new directory path.");
+        path = Console.ReadLine() ?? string.Empty;
+        newDirectory = Directory.CreateDirectory(path);
+    
+        //woops
+       /* for (ushort i = 0; i < folderCount; i++)
+        {
+            ulong filesCount = 0;
+            ulong sizeToCompare = 0;
+            var destinetionFile = newDirectory + "\\" + i;
+            while (sizeToCompare + (ulong) files[filesCount].Length < newSize && (ulong)files.Length >= filesCount)
+            {
+                directory = Directory.CreateDirectory(destinetionFile);
+                string destFile = Path.Combine(directory.FullName, files[filesCount].Name);
+                File.Copy(files[filesCount].FullName, destFile);
+                filesCount++;
+            }
+        }*/
     }
 
-    public static ulong DirSize(DirectoryInfo d)
+    public static ulong DirectorySize(DirectoryInfo directory)
     {
         ulong size = 0;
-        FileInfo[] fis = d.GetFiles();
-        foreach (FileInfo fi in fis)
+        FileInfo[] files = directory.GetFiles();
+        foreach (FileInfo file in files)
         {
-            Console.WriteLine(fi);
-            size += Convert.ToUInt64(fi.Length);
+
+            size += (ulong)(file.Length);
         }
-        DirectoryInfo[] dis = d.GetDirectories();
-        foreach (DirectoryInfo di in dis)
-        {
-            size += DirSize(di);
-        }
+        
         return size;
     }
 }
